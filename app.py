50c38273fa5274d6030431f247c9ef3947531745
ihn-int <69340857+ihn-int@users.noreply.github.com>
2023-12-14 07:17:23 +0800

    Add app.py db.py templates

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
-
-accounts = [1234567, 23456789]
-
+import hashlib
+import db
+users = {
+    "12345678" : "25d55ad283aa400af464c76d713c07ad",
+    "13572468" : "7a386acb9e2b1d09512b5e74934acdff",
+}
 
 app = Flask(__name__)
-
 @app.route("/")
-def hello():
-    return "Hello World"
-
-@app.route('/index')
+@app.route("/index")
 def index():
     return render_template("index.html")
 
@@ -22,17 +21,28 @@ def sign():
 def success():
     return "SUCCESS"
 
+@app.route("/failed")
+def failed():
+    return "FAILED"
+
+@app.route("/unknown")
+def unknown():
+    return "UNKNOWN"
+
 @app.route("/regist", methods = ["POST"])
 def regist():
-    form = request.form
-    
-    #account = form.getkeys("account")
-    #passed = form.getkeys("password")
-    for i, m in form.iteritems():
-        print(i, m)
-    #print(form, account, passed)
-
-    return redirect(url_for("success"))
+    account = request.form["account"]
+    passwd = request.form["password"]
+    if account in users:
+        
+        hash = hashlib.md5(passwd.encode("utf-8")).hexdigest()
+        if users[account] == hash:
+            return redirect(url_for("success"))
+        else:
+            print(hash)
+            return redirect(url_for("failed"))
+    else:
+        return redirect(url_for("unknown"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True, host = "140.113.89.236", port = 5000)
--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
+import hashlib
 
 db = mysql.connector.connect(
     host = "140.113.68.114",
@@ -10,19 +11,22 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-cmd = "SHOW TABLES"
-exec = cursor.execute(cmd)
+
+
+exec = cursor.execute(cmds["favorite"])
 rows = cursor.fetchall()
 
-tables = []
-
-for line in rows:
-    tables.append(line[0])
-
-for i in range(len(tables)):
-    cmd = "SHOW COLUMNS FROM " + tables[i]
-    exec = cursor.execute(cmd)
-    rows = cursor.fetchall()
-    print(tables[0])
-    for line in rows:
-        print(line)
+def check_user_exist(account) -> bool:
+    cmd = "SELECT account in user_table WHERE account == " + str(account)
+    cursor.execute(cmd)
+    cursor.fetchall()
+    if cmd != "":
+        return True
+    
+def check_user_exist(passwd, user_id) -> bool | int:
+    hash = hashlib.md5(passwd.encode("utf-8")).hexdigest()
+    cmd = "SELECT password in user_table WHERE user_id == " + str(user_id)
+    cursor.execute(cmd)
+    cursor.fetchall()
+    if 
+    pass
